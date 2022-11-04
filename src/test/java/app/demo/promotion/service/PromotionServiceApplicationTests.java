a96bcf2d7ffcc55902ca134892227111e72b80cd
Hariprasad Manchi <manchih@vmware.com>
2022-11-04 18:20:29 +0530

    generated spring project using template

---
+++ src/test/java/app/demo/promotion/service/PromotionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package app.demo.promotion.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromotionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
