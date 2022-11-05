64f9c05117a04f9205f61e38d2dffe9310cd145f
Hariprasad Manchi <manchih@vmware.com>
2022-11-05 10:15:49 +0530

    added more test cases

--- src/test/java/app/demo/promotion/service/PromotionServiceApplicationTests.java
+++ src/test/java/app/demo/promotion/service/PromotionServiceApplicationTests.java
@@ -23,10 +23,69 @@ class PromotionServiceApplicationTests {
         List<SKU> skuList = List.of(new SKU("A", 1));
 
         Integer total = promotionService.applyPromotion(skuList);
-
         assertEquals(50, total);
     }
 
+    @Test
+    void when_only_1_item_of_10_quantity_then_total_is_440() {
+
+        List<SKU> skuList = List.of(new SKU("A", 10));
+
+        Integer total = promotionService.applyPromotion(skuList);
+        assertEquals(440, total);
+    }
+
+    @Test
+    void when_only_2_items_of_1_quantity_then_total_is_70() {
+
+        List<SKU> skuList = List.of(new SKU("A", 1),
+                                    new SKU("C", 1));
+
+        Integer total = promotionService.applyPromotion(skuList);
+        assertEquals(70, total);
+    }
+
+    @Test
+    void when_only_2_items_of_10_quantity_then_total_is_640() {
+
+        List<SKU> skuList = List.of(new SKU("A", 10),
+                                    new SKU("C", 10));
+
+        Integer total = promotionService.applyPromotion(skuList);
+        assertEquals(640, total);
+    }
+
+    @Test
+    void when_2_items_of_different_quantity_then_total_is_390() {
+
+        List<SKU> skuList = List.of(new SKU("A", 5),
+                                    new SKU("C", 8));
+
+        Integer total = promotionService.applyPromotion(skuList);
+        assertEquals(390, total);
+    }
+
+    @Test
+    void when_3_items_of_1_quantity_then_total_is_100() {
+
+        List<SKU> skuList = List.of(new SKU("A", 1),
+                                    new SKU("B", 1),
+                                    new SKU("C", 1));
+
+        Integer total = promotionService.applyPromotion(skuList);
+        assertEquals(100, total);
+    }
+
+    @Test
+    void when_3_items_of_5_quantity_then_total_is_450() {
+
+        List<SKU> skuList = List.of(new SKU("A", 5),
+                                    new SKU("B", 5),
+                                    new SKU("C", 5));
+
+        Integer total = promotionService.applyPromotion(skuList);
+        assertEquals(450, total);
+    }
 
     @Test
     void when_4_items_of_1_quantity_then_total_is_110() {
@@ -41,6 +100,78 @@ class PromotionServiceApplicationTests {
     }
 
     @Test
+    void when_3_items_of_1_quantity_with_C_only_then_total_is_140() {
+
+        List<SKU> skuList = List.of(new SKU("A", 1),
+                                    new SKU("B", 1),
+                                    new SKU("C", 3));
+
+        Integer total = promotionService.applyPromotion(skuList);
+        assertEquals(140, total);
+    }
+
+    @Test
+    void when_3_items_of_1_quantity_with_D_only_then_total_is_125() {
+
+        List<SKU> skuList = List.of(new SKU("A", 1),
+                                    new SKU("B", 1),
+                                    new SKU("D", 3));
+
+        Integer total = promotionService.applyPromotion(skuList);
+        assertEquals(125, total);
+    }
+
+    @Test
+    void when_2_items_with_CD_only_then_total_is_30() {
+
+        List<SKU> skuList = List.of(new SKU("C", 1),
+                                    new SKU("D", 1));
+
+        Integer total = promotionService.applyPromotion(skuList);
+        assertEquals(30, total);
+    }
+
+    @Test
+    void when_2_items_of_quantity_3_with_CD_only_then_total_is_90() {
+
+        List<SKU> skuList = List.of(new SKU("C", 3),
+                                    new SKU("D", 3));
+
+        Integer total = promotionService.applyPromotion(skuList);
+        assertEquals(90, total);
+    }
+
+    @Test
+    void when_4_items_of_5_quantity_then_total_is_500() {
+
+        List<SKU> skuList = List.of(new SKU("A", 5),
+                                    new SKU("B", 5),
+                                    new SKU("C", 5),
+                                    new SKU("D", 5));
+
+        Integer total = promotionService.applyPromotion(skuList);
+        assertEquals(500, total);
+    }
+
+    @Test
+    public void when_3_items_of_10_quantity_with_C_only_then_total_is_865() {
+        List<SKU> skuList = List.of(new SKU("A", 10),
+                                    new SKU("B", 10),
+                                    new SKU("C", 10));
+        Integer total = promotionService.applyPromotion(skuList);
+        assertEquals(865, total);
+    }
+
+    @Test
+    public void when_3_items_of_10_quantity_with_D_only_then_total_is_815() {
+        List<SKU> skuList = List.of(new SKU("A", 10),
+                                    new SKU("B", 10),
+                                    new SKU("D", 10));
+        Integer total = promotionService.applyPromotion(skuList);
+        assertEquals(815, total);
+    }
+
+    @Test
     public void when_4_items_of_different_quantity_then_total_is_280() {
 
         List<SKU> skuList = List.of(new SKU("A", 3),
@@ -52,5 +183,20 @@ class PromotionServiceApplicationTests {
         assertEquals(280, total);
     }
 
+    @Test
+    public void when_4_items_of_same_quantity_then_total_is_295() {
+
+        List<SKU> skuList = List.of(new SKU("A", 3),
+                                    new SKU("B", 3),
+                                    new SKU("C", 3),
+                                    new SKU("D", 3));
+
+        /*
+        A:3, B:3, C:3, D:3
+        130 + 45+30 + 30*3 = 295
+         */
+        Integer total = promotionService.applyPromotion(skuList);
+        assertEquals(295, total);
+    }
 
 }
